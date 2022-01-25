9f1ef23baaae1873c315c29149d6cbd18237cf5f
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-01-25 11:20:13 +0000

    user model

--- users/models.py
+++ users/models.py
@@ -13,14 +13,14 @@ class Profile(models.Model):
         return f"{self.user.username}'s profile"
 
     # before saving the image, gets the images and changes the size of the image
-    def save(self):
-        super().save()
-
-        img = Image.open(self.profile_img.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.profile_img.path)
+    #def save(self):
+    #    super().save()
+#
+    #    img = Image.open(self.profile_img.path)
+    #    if img.height > 300 or img.width > 300:
+    #        output_size = (300,300)
+    #        img.thumbnail(output_size)
+    #        img.save(self.profile_img.path)
 
 class Address(models.Model):
     address = models.ForeignKey( Profile, on_delete=models.SET_NULL, null=True)
